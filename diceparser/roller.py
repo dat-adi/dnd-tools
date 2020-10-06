ae210412417d6d9d6a5b87379a80e02be5ee1fba
dat-adi <dat.adithya@gmail.com>
2020-10-06 10:49:53 +0530

    Converted methods into function as self is unused

--- diceparser/roller.py
+++ diceparser/roller.py
@@ -12,25 +12,39 @@ __maintainer__ = "Datta Adithya"
 __email__ = "dat.adithya@gmail.com"
 
 
+# initiationroll rolls for initiative, taking initiation as a param
+def initiationroll(inimod):
+    return randrange(1, 20) + inimod
+
+
+# attackroll rolls for hit/miss, taking ability modifier, and proficiency
+# as parameters.
+def attackroll(abimod, prof):
+    return randrange(1, 20) + abimod + prof
+
+
+# damageroll rolls for damage inflicted,
+# takes weapon damage and ability modifier as parameters
+def damageroll(weapondmg, abimod):
+    return randrange(1, weapondmg) + abimod
+
+
 # Rolling a number
 class Roller:
-    # initiationroll rolls for initiative, taking initiation as a param
-    def initiationroll(self, inimod):
-        return randrange(1, 20) + inimod
+    def __init__(self):
+        self.iniroll = initiationroll(3)
+        self.atkroll = attackroll(3, 2)
+        self.dmgroll = damageroll(5, 3)
 
-    # attackroll rolls for hit/miss, taking ability modifier, and proficiency
-    # as parameters.
-    def attackroll(self, abimod, prof):
-        return randrange(1, 20) + abimod + prof
+    def getiniroll(self):
+        return self.iniroll
 
-    # damageroll rolls for damage inflicted,
-    # takes weapon damage and ability modifier as parameters
-    def damageroll(self, weapondmg, abimod):
-        return randrange(1, weapondmg) + abimod
+    def getatkroll(self):
+        return self.atkroll
+
+    def getdmgroll(self):
+        return self.dmgroll
 
 
 if __name__ == "__main__":
     roller = Roller()
-    print("Initiation roll : ", roller.initiationroll(3))
-    print("Attack roll : ", roller.attackroll(3, 2))
-    print("Damage roll : ", roller.damageroll(5, 3))
